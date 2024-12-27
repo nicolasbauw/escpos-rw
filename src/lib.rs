b34dffd69b5531acc0ff085dc9777a22c04aac7e
Nicolas BAUW <nbauw@hotmail.com>
2024-12-27 10:10:46 +0100

    Doc update

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,9 @@
 //! escpos-rw: USB communication with ESC/POS (Epson) thermal printers
 //! 
-//! Escpos-rw is a "low level" fork of escpos-rs, aiming at providing just the USB connection and read/write functions.
+//! escpos-rw is a "low level" fork of escpos-rs, aiming at providing the USB connection and read/write functions.
 //! - bug fixes
 //! - cleaning unused and unwanted code
 //! - adding read functions
-//! 
-//! ```rust
-//! // VID/PID parameters
-//! let Some(printer) = Printer::new(0x04b8, 0x04b8)? else {
-//!     return Err(escpos_rw::Error::PrinterError(
-//!         "No printer found !".to_string(),
-//!     ));
-//! };
-//! ```
 
 pub use printer::Printer;
 pub use error::Error;
