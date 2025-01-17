8c55013299f5f5d517a7708d1dc9a79ad13468e4
Nicolas BAUW <nbauw@hotmail.com>
2025-01-17 14:05:33 +0100

    Better error messages

--- src/error.rs
+++ src/error.rs
@@ -12,9 +12,9 @@ pub enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         let content = match self {
-            Error::UsbError(e) => format!("rusb error: {}", e),
+            Error::UsbError(e) => format!("USB error: {}", e),
             Error::NoBulkEndpoint => "No bulk endpoint could be found".to_string(),
-            Error::PrinterError(detail) => format!("An error occured while printing, {}", detail),
+            Error::PrinterError(detail) => format!("Printer error: {}", detail),
         };
         write!(formatter, "{}", content)
     }
