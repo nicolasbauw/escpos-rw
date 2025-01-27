2f785bb8fe916103bb900c0fc4a75608a53e2159
Nicolas BAUW <nbauw@hotmail.com>
2025-01-27 15:37:06 +0100

    IoError

--- src/error.rs
+++ src/error.rs
@@ -1,12 +1,20 @@
 /// Errors that this crate throws.
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Error {
     /// Error related to rusb
     UsbError(rusb::Error),
     /// This means no bulk endpoint could be found
     NoBulkEndpoint,
+    /// I/O Error
+    IoError,
     /// Generic error
     PrinterError(String),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_e: std::io::Error) -> Error {
+        Error::IoError
+    }
 }
 
 impl std::fmt::Display for Error {
@@ -14,6 +22,7 @@ impl std::fmt::Display for Error {
         let content = match self {
             Error::UsbError(e) => format!("USB error: {}", e),
             Error::NoBulkEndpoint => "No bulk endpoint could be found".to_string(),
+            Error::IoError => format!("I/O Error"),
             Error::PrinterError(detail) => format!("Printer error: {}", detail),
         };
         write!(formatter, "{}", content)
